bcf5698a7d046b6821b45e73f6617a383d7f89d7
vonschtirlitz <nikita.usenko@gmail.com>
2016-05-10 14:28:19 -0700

    Added Spencer's nongui MainStart code
    added spencers nongui code

--- Careers Code 5-10-16/MainStart.java
+++ Careers Code 5-10-16/MainStart.java
@@ -2,14 +2,19 @@
 //getNumPlayers()
 public class MainStart
 {
+    private int playerNum;
     public MainStart()
     {
+        System.out.println("Welcome to the game of Careers!");
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Please enter the total number of players. \nPlease remember that your number of players MUST be between 2-6.");
+        playerNum = reader.nextInt();
+        System.out.println("Thank you");
     }
     
     public int getNumPlayers()
     {
-        return 4;
-        //this will be fixed maybe
+        return this.playerNum;
     }
 }
         
